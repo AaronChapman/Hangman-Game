25339f8a3547f8456a95f976aa222c61914b1c43
oylo <oylomusic@gmail.com>
2018-02-28 14:57:22 -0500

    l

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,6 +24,7 @@ function new_game() {
     $(".guesses_left").text("guesses left: 0");
     $(".correct_letters_guessed").text("correct guesses: ");
     $(".incorrect_letters_guessed").text("incorrect guesses: ");
+    $('*').css('text-shadow', '1px 1px #545454');
 
     /* choose a new word & start background color animation loop */
     new_word();
